02c7df91850bcdfce5a61c1fb023e43a7ca21b8a
Dhandapani Devarajan <dhandapani.devarajan@imaginea.com>
2019-07-16 15:21:10 +0530

    Added Add Book View

---
+++ iRead/Views/AddBookView.swift
@@ -0,0 +1,23 @@
+//
+//  AddBookView.swift
+//  iRead
+//
+//  Created by Dhandapani Devarajan on 16/07/19.
+//  Copyright © 2019 imaginea. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddBookView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct AddBookView_Previews : PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
+#endif
